0510727d58a355c9ed9dcb11ef677137e0348955
Theaidable <theaidable@hotmail.dk>
2024-10-01 13:17:41 +0200

    plebb
    beepboop

--- ConsoleSkak/Program.cs
+++ ConsoleSkak/Program.cs
@@ -1,8 +1,9 @@
-﻿//using System;
-//using System.Collections.Generic;
-//using System.Linq;
-//using System.Text;
-//using System.Threading.Tasks;
+﻿
+
+// Quit funktion
+// Try again/reset funktion - o
+// Farve på "brikkerne"
+// Return to main menu funktion
 
 //namespace ConsoleSkak
 //{
